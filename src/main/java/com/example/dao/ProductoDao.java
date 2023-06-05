f1dea1ce3b5762e035aa6ec823cd5b803fec29ee
yakouta <yakouta.a@hotmail.es>
2023-06-05 12:56:22 +0200

    antes de crear el controlador del producto

---
+++ src/main/java/com/example/dao/ProductoDao.java
@@ -0,0 +1,42 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.entities.Producto;
+
+public interface ProductoDao extends JpaRepository<Producto, Integer> {
+    /**
+     * vamos a necesitar tres metodos personalizados
+     * que traigan la pñresentacion en una consulta, que es mas eficeinte,
+     * que primero traer el producto y luego una subconsulta para la presentacion
+     * que es lo que por defecto ocurriria por esta establecido al fetch type lazy
+     * 1. recuperará los productos paginados, es decir de 10 en 10, de 20 en 20 etc
+     * 
+     * 2. otro que recupera los productos ordenados, sin paginacion
+     * 
+     * 3. dado el id de un producto recupera el producto con su presentacion correspondiente
+     * para ello vamos a utilizar el lenguaje HQL(hybernate query language), es muy similar a SQL, pero los se consulta 
+     * son las entidades, no las tablas.
+     * y no se utiliza consultas de SQL nativo porque no sopòrtan la paginacion y el ordenamiento.
+     */
+
+
+     // el metodo siguiente ademas de paginacion tambien ofrece ordenamiento
+     @Query(value = "select p from Producto p left join fetch p.presentacion",
+     countQuery = "select count(p) from Producto p left join p.presentaacion")
+     public Page<Producto> findAll(Pageable pageable);
+
+
+     @Query(value = "select p from Producto p left join fetch p.presentacion")
+     public List<Producto>findAll(Sort sort);
+
+     @Query(value = "select p from Producto p left join fetch p.presentacion where p.id = :id")
+     public Producto findById(int id);
+    
+}
---
+++ src/main/java/com/example/entities/Presentacion.java
@@ -0,0 +1,40 @@
+package com.example.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "presentaciones")
+public class Presentacion implements Serializable{
+
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.PERSIST, mappedBy = "presentacion")
+    private List<Producto> productos;
+
+
+}
